96b6a233fbe4d31c82fde9698e5c18c947dc66b6
Daniil Maksimov <d.maksimov@xlab-game.com>
2023-10-10 15:13:48 +0300

    HW3,4+

---
+++ Assets/Scripts/SwapTools.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WorkProject
+{
+    public class SwapTools : MonoBehaviour
+    {
+        public GameObject[] tools;
+
+        public void Swap()
+        {
+            tools.SetActive(false);
+        }
+
+
+    }
+}
